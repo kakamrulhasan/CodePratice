b9865a4b4b5aaa85bef04db3b4744a72d1abca7a
kamrul <kakamrul2000@gmail.com>
2025-11-20 01:54:14 +0600

    syntex

---
+++ dart_basic/syntex.dart
@@ -0,0 +1,10 @@
+void main() {
+  //void mean no return value
+  var name = 'dart'; //var will handel dynamic value
+  print(name);
+  print('hello $name');
+
+  //final and const
+  final String langugage = 'dart'; // final will be initialized at runtime
+  const pi = 3.1415; // must be initialized at compile time
+}
