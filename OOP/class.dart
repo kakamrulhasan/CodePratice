4e80ee0a694731065be2f91fcba364b0937f9dd0
kamrul <kakamrul2000@gmail.com>
2025-11-21 01:45:24 +0600

    object_print

--- OOP/class.dart
+++ OOP/class.dart
@@ -13,10 +13,15 @@ void main() {
   student1.name = 'x';
   student1.roll = 54;
   student1.salary = 2.2;
-
+  print("Name: ${student1.name}");
+  print("Roll: ${student1.roll}");
+  print("Salary: ${student1.salary}");
   // 2nd object
   student student2 = student();
   student2.name = 'y';
   student2.roll = 23;
   student2.salary = 2.5;
+  print(
+    'name: ${student2.name}, \nroll:${student2.roll},\nsalary: ${student2.salary}',
+  );
 }
