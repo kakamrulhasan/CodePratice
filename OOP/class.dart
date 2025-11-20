f364f08c4444aff7fe74c3b544c096ba09519d43
kamrul <kakamrul2000@gmail.com>
2025-11-21 01:40:35 +0600

    add_object

--- OOP/class.dart
+++ OOP/class.dart
@@ -1,7 +1,22 @@
 class student {
   // user define data type is called class
 
-  String? name;
+  String?
+  name; // every tree are class over the world ; the individual tree like mango, orange, coconut tree we can called object ;
   int? roll;
   double? salary;
+}
+
+void main() {
+  // 1st object
+  student student1 = student();
+  student1.name = 'x';
+  student1.roll = 54;
+  student1.salary = 2.2;
+
+  // 2nd object
+  student student2 = student();
+  student2.name = 'y';
+  student2.roll = 23;
+  student2.salary = 2.5;
 }
